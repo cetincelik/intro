272f7b7de626cda5bb905ecc40987cfbec95c431
cetincelik <cetincelik026@gmail.com>
2022-04-17 12:27:33 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Row, Col } from "reactstrap";
+import CategoryList from "./CategoryList";
+import Navi from "./Navi";
+import ProductList from "./ProductList";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <Row>
+           <Navi/>
+        </Row>
+        <Row>
+          <Col xs="3"> 
+          <CategoryList title="Category List" />
+          </Col>
+          <Col xs="9">
+          <ProductList title="Product List" />
+          </Col>
+       
+       
+        </Row>
+      </Container>
+
+    
+    
     </div>
   );
 }
