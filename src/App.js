bd263d1c604037e0782c665a39ead57c63cf40af
Çetin Çelik <cetincelik026@gmail.com>
2022-04-21 13:12:40 +0300

    coding continued

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
+import { Component } from "react";
 import { Container, Row, Col } from "reactstrap";
 import CategoryList from "./CategoryList";
 import Navi from "./Navi";
 import ProductList from "./ProductList";
 
-function App() {
-  let ProductInfo = { title: "Product List" };
-  let CategoryInfo = { title: "Category List" };
-  return (
-    <div>
-      <Container>
-        <Row>
-          <Navi />
-        </Row>
-        <Row>
-          <Col xs="3">
-            <CategoryList info={CategoryInfo} />
-          </Col>
-          <Col xs="9">
-            <ProductList info={ProductInfo} />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+export default class App extends Component {
+state={currentCategory:"",products:[]}
+
+componentDidMount(){
+  this.getProducts();
 }
 
-export default App;
+  changeCategory = category =>{
+    this.setState({currentCategory:category.categoryName});
+    this.getProducts(category.id);
+  }
+
+  getProducts = categoryId => {
+    let url = "http://localhost:3000/products";
+    if (categoryId) {
+      url += "?categoryId=" + categoryId;
+    }
+
+    fetch(url)
+    .then(response=>response.json())
+    .then(data=>this.setState({products:data}));;
+  }
+
+  render() {
+    let productInfo = { title: "Product List" };
+    let categoryInfo = { title: "Category List" };
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi />
+          </Row>
+          <Row>
+            <Col xs="3">
+              <CategoryList 
+              currentCategory={this.state.currentCategory} 
+              changeCategory={this.changeCategory} 
+              info={categoryInfo} />
+            </Col>
+            <Col xs="9">
+              <ProductList
+                products={this.state.products}
+              currentCategory={this.state.currentCategory} 
+              info={productInfo} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
--- src/Navi.js
+++ src/Navi.js
@@ -1,11 +1,77 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { 
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  NavbarText
+} from 'reactstrap';
 
 export default class Navi extends Component {
   render() {
     return (
       <div>
-          <h2>Navi Component</h2>
-       </div>
+  <Navbar
+    color="light"
+    expand="md"
+    light
+  >
+    <NavbarBrand href="/">
+      reactstrap
+    </NavbarBrand>
+    <NavbarToggler onClick={function noRefCheck(){}} />
+    <Collapse navbar>
+      <Nav
+        className="me-auto"
+        navbar
+      >
+        <NavItem>
+          <NavLink href="/components/">
+            Components
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="https://github.com/reactstrap/reactstrap">
+            GitHub
+          </NavLink>
+        </NavItem>
+        <UncontrolledDropdown
+          inNavbar
+          nav
+        >
+          <DropdownToggle
+            caret
+            nav
+          >
+            Options
+          </DropdownToggle>
+          <DropdownMenu right>
+            <DropdownItem>
+              Option 1
+            </DropdownItem>
+            <DropdownItem>
+              Option 2
+            </DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem>
+              Reset
+            </DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </Nav>
+      <NavbarText>
+        Simple Text
+      </NavbarText>
+    </Collapse>
+  </Navbar>
+</div>
     )
   }
 }
