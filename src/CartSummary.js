334e845d51a4bd660848f4808714a179d9540425
Çetin Çelik <cetincelik026@gmail.com>
2022-04-23 10:18:44 +0300

    coding continued

--- src/CartSummary.js
+++ src/CartSummary.js
@@ -1,31 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-    
-    UncontrolledDropdown,
-    DropdownItem,
-    DropdownMenu,
-    DropdownToggle,
-    
-  } from "reactstrap";
+  UncontrolledDropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Badge,
+  NavItem,
+  NavLink,
+} from "reactstrap";
 
 export class CartSummary extends Component {
+  renderSummary() {
+    return (
+      <UncontrolledDropdown inNavbar nav>
+        <DropdownToggle caret nav>
+          Your Cart - {this.props.cart.length}
+        </DropdownToggle>
+        <DropdownMenu right>
+          {this.props.cart.map((cartItem) => (
+            <DropdownItem key={cartItem.product.id}>
+              <Badge color="danger" onClick={()=>this.props.removeFromCart(cartItem.product)} >X</Badge>
+              {cartItem.product.productName}
+              <Badge color="success">{cartItem.quantity}</Badge>
+            </DropdownItem>
+          ))}
+
+          <DropdownItem divider />
+          <DropdownItem>Reset</DropdownItem>
+        </DropdownMenu>
+      </UncontrolledDropdown>
+    );
+  }
+  renderEmptyCart() {
+    return (
+      <NavItem>
+        <NavLink>Empty Cart</NavLink>
+      </NavItem>
+    );
+  }
+
+ 
   render() {
     return (
-      <div>
-          <UncontrolledDropdown inNavbar nav>
-                <DropdownToggle caret nav>
-                  Options - {this.props.cart.length}
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>Option 1</DropdownItem>
-                  <DropdownItem>Option 2</DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>Reset</DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-      </div>
-    )
+      <div>{this.props.cart.length > 0 ? this.renderSummary() : this.renderEmptyCart()}</div>
+    );
   }
 }
 
-export default CartSummary
+export default CartSummary;
